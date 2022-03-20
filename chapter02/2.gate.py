ba8c35bcbbd692c9f62b0cc06a5d6675edcfb277
Liu Rui <lovearuis@gmail.com>
2022-03-20 20:54:38 +0800

    add XOR

--- chapter02/2.gate.py
+++ chapter02/2.gate.py
@@ -11,12 +11,14 @@ def AND(x1, x2):
     else:
         return 1
 
+
 print(AND(0, 0))
 print(AND(1, 0))
 print(AND(0, 1))
 print(AND(1, 1))
 
 print("===")
+
 
 def NAND(x1, x2):
     x = np.array([x1, x2])
@@ -28,12 +30,14 @@ def NAND(x1, x2):
     else:
         return 1
 
+
 print(NAND(0, 0))
 print(NAND(1, 0))
 print(NAND(0, 1))
 print(NAND(1, 1))
 
 print("===")
+
 
 def OR(x1, x2):
     x = np.array([x1, x2])
@@ -45,7 +49,23 @@ def OR(x1, x2):
     else:
         return 1
 
+
 print(OR(0, 0))
 print(OR(1, 0))
 print(OR(0, 1))
 print(OR(1, 1))
+
+print("===")
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+
+print(XOR(0, 0))
+print(XOR(1, 0))
+print(XOR(0, 1))
+print(XOR(1, 1))
