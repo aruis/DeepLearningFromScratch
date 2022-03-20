d6593c966bd9f3efc353385912949f840c35e058
Liu Rui <lovearuis@gmail.com>
2022-03-20 10:52:51 +0800

    learn numpy over

--- chapter01/2.learnNumPy.py
+++ chapter01/2.learnNumPy.py
@@ -40,3 +40,21 @@ B = np.array([
 
 print(A * B)
 
+X = np.array([
+    [51, 55],
+    [14, 19],
+    [0, 4]
+])
+
+for row in X:
+    print(row)
+
+X = X.flatten()
+
+print(X)
+
+print(X[np.array([0, 2, 4])])
+
+print(X > 15)
+
+print(X[X > 15])
